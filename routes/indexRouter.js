ae9f643b17abcc00cae0de0e3918ba5426e8dce7
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-10-18 19:58:25 +0800

    Added basic MVC structure and an index router

---
+++ routes/indexRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const indexRouter = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
