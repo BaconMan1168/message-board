b16037452be2b3162f2f02875b9e67678d243eb5
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-10-18 20:53:37 +0800

    Created seperate data module so both routers can access the same messages array

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use('/', indexRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use('/new', newRouter);
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,3 +1,12 @@
 const { Router } = require('express')
 const newRouter = Router();
 
+
+
+newRouter.get('/form', (req, res) => {
+    res.render('form');
+})
+
+newRouter.post('/', (req, res) => {
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+})
