716c7ecb7ba631b5ef90dd4256686023d4b2ed75
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-10-18 21:12:10 +0800

    Added functional navbar to navigate from homepage to the new message form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,13 @@ const { Router } = require('express');
 const indexRouter = Router();
 const { messages } = require('../data/messageData') 
 
+const links = [
+    {href: '/', text: 'Home'},
+    {href: '/new/form', text: "New Message"}
+]
+
 indexRouter.get('/', (req, res) => {
-    res.render('index', { messages: messages})
+    res.render('index', { messages: messages, 'links': links })
 })
 
 module.exports = indexRouter;
